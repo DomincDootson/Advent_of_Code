40c181cc9f23238a7688bc4eb5a801138bce0ffc
DominicDootson <Dominicdootson@gmail.com>
2023-12-13 18:05:59 +0000

    Tidied up the day 9 solution

--- mirage_maintenance_day_9.py
+++ mirage_maintenance_day_9.py
@@ -1,13 +1,12 @@
 def read_values(filename):
-	with open(filename) as file:
-		sequences = [[int(v) for v in line.split()] for line in file.readlines()]
-
+	with open(filename) as file: sequences = [[int(v) for v in line.split()] for line in file.readlines()]
 	return sequences
 
-def make_prediction_forwards(sequence):
-	if len(set(sequence)) == 1 and sequence[0] ==0:
-		return 0
+def check_zeros(sequence):
+	return len(set(sequence)) == 1 and sequence[0] == 0
 
+def make_prediction_forwards(sequence):
+	if check_zeros(sequence): return 0
 	return sequence[-1] + make_prediction_forwards([f - b for f, b in zip(sequence[1:], sequence)])
 
 def make_predictions_forwards(filename):
@@ -15,15 +14,13 @@ def make_predictions_forwards(filename):
 	return sum((make_prediction_forwards(seq) for seq in sequences))
 
 def make_prediction_backwards(sequence):
-	if len(set(sequence)) == 1 and sequence[0] ==0:
-		return 0
-
+	if check_zeros(sequence): return 0
 	return sequence[0] - make_prediction_backwards([f - b for f, b in zip(sequence[1:], sequence)])
 
 def make_predictions_backwards(filename):
 	sequences = read_values(filename)
 	return sum((make_prediction_backwards(seq) for seq in sequences))
-	
+
 print(make_predictions_forwards("Inputs/Day_9_Test.txt"))
 print(make_predictions_forwards("Inputs/Day_9_input.txt"))
 
